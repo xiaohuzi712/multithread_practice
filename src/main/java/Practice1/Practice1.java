cba60716b7b69eab86c076cddc3b31d7f683563f
huchao <1083763917@qq.com>
2017-06-29 22:56:21 +0800

    项目提交文件测试2

---
+++ src/main/java/Practice1/Practice1.java
@@ -0,0 +1,10 @@
+package Practice1;
+
+/**
+ * Created by huchao on 2017/6/29.
+ */
+public class Practice1 {
+    public static void main(String[] args) {
+        System.out.println("main目录下的文件被提交");
+    }
+}
